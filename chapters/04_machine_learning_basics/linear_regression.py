c2629f6e627f65d35d86ed0d597f7f7c4862380b
郝宁 <haoning02@meituan.com>
2019-03-06 17:33:25 +0800

    linear_regression.py

--- chapters/04_machine_learning_basics/linear_regression.py
+++ chapters/04_machine_learning_basics/linear_regression.py
@@ -16,7 +16,7 @@ def loss(X, Y):
 
 
 def inputs():
-    # Data from http://people.sc.fsu.edu/~jburkardt/datasets/regression/x09.txt
+    # Data from http://people.sc.fsu.edu/~jburkardt/datasets/regression/x09.txt 
     weight_age = [[84, 46], [73, 20], [65, 52], [70, 30], [76, 57], [69, 25], [63, 28], [72, 36], [79, 57], [75, 44], [27, 24], [89, 31], [65, 52], [57, 23], [59, 60], [69, 48], [60, 34], [79, 51], [75, 50], [82, 34], [59, 46], [67, 23], [85, 37], [55, 40], [63, 30]]
     blood_fat_content = [354, 190, 405, 263, 451, 302, 288, 385, 402, 365, 209, 290, 346, 254, 395, 434, 220, 374, 308, 220, 311, 181, 274, 303, 244]
 
@@ -29,10 +29,10 @@ def train(total_loss):
 
 
 def evaluate(sess, X, Y):
-    print sess.run(inference([[50., 20.]])) # ~ 303
-    print sess.run(inference([[50., 70.]])) # ~ 256
-    print sess.run(inference([[90., 20.]])) # ~ 303
-    print sess.run(inference([[90., 70.]])) # ~ 256
+    print(sess.run(inference([[50., 20.]]))) # ~ 303
+    print(sess.run(inference([[50., 70.]]))) # ~ 256
+    print(sess.run(inference([[90., 20.]]))) # ~ 303
+    print(sess.run(inference([[90., 70.]]))) # ~ 256
 
 # Launch the graph in a session, setup boilerplate
 with tf.Session() as sess:
@@ -52,9 +52,9 @@ with tf.Session() as sess:
     for step in range(training_steps):
         sess.run([train_op])
         if step % 1000 == 0:
-            print "Epoch:", step, " loss: ", sess.run(total_loss)
+            print("Epoch:", step, " loss: ", sess.run(total_loss))
 
-    print "Final model W=", sess.run(W), "b=", sess.run(b)
+    print("Final model W=", sess.run(W), "b=", sess.run(b))
     evaluate(sess, X, Y)
 
     coord.request_stop()
